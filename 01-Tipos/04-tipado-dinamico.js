9d0dbf673b9d14968266185b9aed0a55f2e1c1ca
Joaquin <joaquincalligaro@gmail.com>
2025-06-29 23:15:06 -0300

    Add initial HTML structure and JavaScript files for variables, primitives, constants, dynamic typing, comments, and main app

---
+++ 01-Tipos/04-tipado-dinamico.js
@@ -0,0 +1,11 @@
+let numero = 42; // Number
+let texto = 'Hola, mundo'; // String
+let verdadero = true; // Boolean
+let undef; // Undefined
+let nula = null; // Null
+
+console.log(typeof numero);
+console.log(typeof texto);
+console.log(typeof verdadero);
+console.log(typeof undef);
+console.log(typeof nula);
---
+++ 01-Tipos/05-comentarios.js
@@ -0,0 +1,17 @@
+// Este es otro comentario
+
+/*
+Todo lo que escribamos entre los slash 
+y los astericos 
+va a ser ignorado
+*/
+
+/**
+ * Este es un comentario
+ * de
+ * varias líneas
+ *
+ */
+let numero = 42;
+
+console.log(numero);
