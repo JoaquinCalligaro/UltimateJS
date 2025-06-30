067820335b5601326dd4b99670ce8196f71e09ef
Joaquin <joaquincalligaro@gmail.com>
2025-06-30 12:23:11 -0300

    Update script reference in HTML and add assignment operations in JavaScript

---
+++ 02-Operadores/02-asignacion.js
@@ -0,0 +1,10 @@
+let a = 5;
+a = a + 5;
+
+a += 5; // Suma 5 al valor actual de 'a' (a = a + 5)
+a -= 5; // Resta 5 al valor actual de 'a' (a = a - 5)
+a *= 5; // Multiplica el valor actual de 'a' por 5 (a = a * 5)
+a /= 5; // Divide el valor actual de 'a' entre 5 (a = a / 5)
+a %= 5; // Calcula el resto de la división del valor actual de 'a' entre 5 (a = a % 5)
+a **= 5; // Eleva el valor actual de 'a' a la potencia de 5 (a = a ** 5)
+console.log(a);
