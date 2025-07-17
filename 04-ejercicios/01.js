f76d77eda86b86bcb8afe4ce02a525634099b8e8
Joaquin Calligaro <joaquincalligaro@gmail.com>
2025-07-17 19:50:39 -0300

    Ejercicio 1 completado!

---
+++ 04-ejercicios/01.js
@@ -0,0 +1,17 @@
+let a = 25;
+let b = 10;
+
+// function cualEsMayor(a, b) {
+//   if (a > b) {
+//     return a;
+//   } else {
+//     return b;
+//   }
+// }
+// console.log(cualEsMayor(a, b));
+
+// operador ternario
+function cualEsMayor(a, b) {
+  return a > b ? a : b;
+}
+console.log(cualEsMayor(a, b));
