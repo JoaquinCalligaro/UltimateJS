75d83b8ac05899e23db4c502e7c949613d15727c
Joaquin Calligaro <joaquincalligaro@gmail.com>
2025-07-24 00:47:11 -0300

    Ejercicio 04 Completado!

---
+++ 04-ejercicios/04.js
@@ -0,0 +1,20 @@
+// Ejercicio 4: Escribir un algoritmo que imprima los numeros impares del 0 al 10
+
+let numero = 0;
+
+while (numero < 10) {
+  numero++;
+  if (numero % 2 !== 0) {
+    console.log(numero, 'Impar');
+  }
+}
+
+//! inverso (descomentar lineas)
+// let numero = 10;
+
+// while (numero >= 0) {
+//   numero--;
+//   if (numero % 2 !== 0) {
+//     console.log(numero, 'Impar');
+//   }
+// }
